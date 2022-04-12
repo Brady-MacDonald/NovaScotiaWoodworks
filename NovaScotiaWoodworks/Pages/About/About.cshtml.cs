df65c0df303b9e9de27d86c11dc379d75b31206c
Brady-MacDonald <73553359+Brady-MacDonald@users.noreply.github.com>
2022-04-12 14:51:10 -0300

    Creating the About page

--- NovaScotiaWoodworks/Pages/About/About.cshtml.cs
+++ NovaScotiaWoodworks/Pages/About/About.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace NovaScotiaWoodworks.Pages
+namespace NovaScotiaWoodworks.Pages.About
 {
     public class AboutModel : PageModel
     {
