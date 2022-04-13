407bb71a750cb1032df36b25b1ad4bd98b87ffe6
Brady-MacDonald <73553359+Brady-MacDonald@users.noreply.github.com>
2022-04-12 21:56:36 -0300

    Added design for Tables and Mantels page

--- NovaScotiaWoodworks/Pages/Products/Tables.cshtml.cs
+++ NovaScotiaWoodworks/Pages/Products/Tables.cshtml.cs
@@ -1,12 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NovaScotiaWoodworks.Data;
+using NovaScotiaWoodworks.Models;
+using System.Threading.Tasks;
 
 namespace NovaScotiaWoodworks.Pages.Products
 {
     public class TablesModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public OrderModel Order { get; set; }
+        
+        public TablesModel(ApplicationDbContext db)
+        {
+            db = _db;
+            Order = new OrderModel();
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!User.Identity.IsAuthenticated)
+            {
+                ModelState.AddModelError("OrderError", "Must be signed in to place order");
+                return Page();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("NoAccount", "Invalid input");
+                return Page();
+            }
+
+            try
+            {
+                _db.Orders.Add(Order);
+                _db.SaveChanges();
+            }
+            catch
+            {
+                ModelState.AddModelError("OrderError", "Unable to place order");
+                return Page();
+            }
+
+            return Redirect("/Products/Tables");
+
         }
     }
 }
