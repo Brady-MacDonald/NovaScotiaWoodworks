edf057b1899913da5f3cfa2ae87e090641c2e6de
Brady-MacDonald <73553359+Brady-MacDonald@users.noreply.github.com>
2022-04-13 15:30:29 -0300

    Add ability to change password

---
+++ NovaScotiaWoodworks/Pages/Account/ChangePassword.cshtml.cs
@@ -0,0 +1,75 @@
+using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NovaScotiaWoodworks.AccountManager;
+using NovaScotiaWoodworks.Data;
+using NovaScotiaWoodworks.Models;
+
+namespace NovaScotiaWoodworks.Pages.Account
+{
+    [Authorize]
+    public class ChangePasswordModel : PageModel
+    {
+        [BindProperty]
+        public PasswordChangeModel ChangePassword { get; set; }
+        public UserModel CurrentUser { get; set; }
+        private readonly ApplicationDbContext _db;
+        private readonly INotyfService _notyf;
+
+        public ChangePasswordModel(ApplicationDbContext db, INotyfService notyf)
+        {
+            _db = db;
+            _notyf = notyf;
+            CurrentUser = new UserModel();
+            ChangePassword = new PasswordChangeModel();
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //Get the users currently stored password hash
+            CurrentUser = _db.Users.Find(User.Identity.Name);
+
+            //Convert the user entered password into its hash
+            string currentPasswordHash = PasswordHash.GetStringSha256Hash(ChangePassword.CurrentPassword);
+
+            if(CurrentUser.Password != currentPasswordHash)
+            {
+                ModelState.AddModelError("PasswordError", "Incorrect password");
+                return Page();
+            }
+
+            if (ChangePassword.NewPassword != ChangePassword.ConfirmPassword)
+            {
+                ModelState.AddModelError("PasswordError", "Passwords did not match");
+                return Page();
+            }
+
+            //User has confirmed old password and entered matching new password
+            //Proceed with updating the stored password in db
+            string newPasswordHash = PasswordHash.GetStringSha256Hash(ChangePassword.NewPassword);
+            CurrentUser.Password = newPasswordHash;
+            try
+            {
+                _db.Users.Update(CurrentUser);
+                _db.SaveChanges();
+            }
+            catch
+            {
+                _notyf.Error("Unable to update");
+                return Page();
+            }
+            _notyf.Success("Password Updated");
+            return Redirect("/Index");
+        }
+    }
+}
